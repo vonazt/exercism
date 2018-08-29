8536313bb9e5476d61bb94db287e46db6892fead
Richard Tzanov <richardtzanov@gmail.com>
2018-08-29 10:24:42 +0100

    refactored and resubmitted solution - moved all functions into Bob class

--- bob/bob.js
+++ bob/bob.js
@@ -1,24 +1,30 @@
 class Bob {
+
   hey(phrase) {
-    phrase = phrase.replace(/[^a-zA-Z|0-9|?]/g, '');
-    const phraseType = phrase.slice(-1);
-    if(!phrase.length) return 'Fine. Be that way!';
-    if(isUpperCase(phrase) && phraseType !== '?' && !isNumbers(phrase)) return 'Whoa, chill out!';
-    if(phraseType === '?') return checkQuestion(phrase);
+    this.phrase = phrase;
+    this.phrase = this.removeSuperfluousSpacesAndCharacters(this.phrase);
+    const phraseType = this.phrase.slice(-1);
+    if(!this.phrase.length) return 'Fine. Be that way!';
+    if(this.isUpperCase(this.phrase) && phraseType !== '?' && !this.isNumbers(this.phrase)) return 'Whoa, chill out!';
+    if(phraseType === '?') return this.checkQuestion(this.phrase);
     else return 'Whatever.';
   }
-}
 
-function isUpperCase(phrase) {
-  return phrase === phrase.toUpperCase();
-}
+  removeSuperfluousSpacesAndCharacters(phrase) {
+    return phrase.replace(/[^a-zA-Z|0-9|?]/g, '');
+  }
 
-function isNumbers(phrase) {
-  return !/\D/.test(phrase);
-}
+  isUpperCase(phrase) {
+    return phrase === phrase.toUpperCase();
+  }
 
-function checkQuestion(phrase) {
-  return !isUpperCase(phrase) || isNumbers(phrase.slice(0, -1)) ? 'Sure.' : 'Calm down, I know what I\'m doing!';
+  isNumbers(phrase) {
+    return !/\D/.test(phrase);
+  }
+
+  checkQuestion(phrase) {
+    return !this.isUpperCase(phrase) || this.isNumbers(phrase.slice(0, -1)) ? 'Sure.' : 'Calm down, I know what I\'m doing!';
+  }
 }
 
 module.exports = Bob;
